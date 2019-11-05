204aa02e451ca63091c401a69881914ec4db474c
Gujjar1998 <dk2984663@gmail.com>
2019-11-05 14:30:15 +0530

    recent changes

---
+++ Hacker Earth/Basic of IO/Docter's secret.py
@@ -0,0 +1,8 @@
+l,n=map(int,input().split())
+if l<=23:
+    if(n>=500 and n<=1000):
+        print("Take Medicine")
+    else:
+        print("Don't take Medicine")
+else:
+    print("Don't take Medicine")
